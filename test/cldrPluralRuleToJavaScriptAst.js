0dfa78d6be12d7445f234dafc294708626a510ed
Andreas Lind Petersen <andreas@one.com>
2013-11-18 22:33:01 +0100

    Test: Replace expect.js with unexpected.

--- test/cldrPluralRuleToJavaScriptAst.js
+++ test/cldrPluralRuleToJavaScriptAst.js
@@ -1,20 +1,22 @@
-var expect = require('expect.js'),
+var unexpected = require('unexpected'),
     uglifyJs = require('uglify-js'),
     cldrPluralRuleToJavaScriptAst = require('../lib/cldrPluralRuleToJavaScriptAst');
 
 describe('cldrPluralRuleToJavaScriptAst', function () {
-    // expectedOutput is a string of JavaScript source code rather than an AST so the tests can be more compact:
-    function createTestCase(cldrPluralRuleStr, expectedOutput) {
-        it(cldrPluralRuleStr, function () {
-            expect(uglifyJs.uglify.gen_code(cldrPluralRuleToJavaScriptAst(cldrPluralRuleStr), {beautify: true})).to.equal(expectedOutput);
-        });
-    }
+    var expect = unexpected.clone();
 
-    createTestCase('n is 4 or n is not 6', 'n === 4 || n !== 6');
-    createTestCase(' n  is  4  or  n  is  not  6 ', 'n === 4 || n !== 6');
-    createTestCase('n within 2..4, 4..6', 'n >= 2 && n <= 4 || n >= 4 && n <= 6');
-    createTestCase('n in 2..4, 4..6', 'n === Math.floor(n) && (n >= 2 && n <= 4 || n >= 4 && n <= 6)');
-    createTestCase('n within 2, 4', 'n === 2 || n === 4');
-    createTestCase('n is not 2', 'n !== 2');
-    createTestCase('n mod 100 in 11..99', 'n % 100 === Math.floor(n % 100) && n % 100 >= 11 && n % 100 <= 99');
+    // `value` is a string of JavaScript source code rather than an AST so the tests can be more compact:
+    expect.addAssertion('[not] to encode to', function (value) {
+        expect(uglifyJs.uglify.gen_code(cldrPluralRuleToJavaScriptAst(this.obj), {beautify: true}), 'to equal', value);
+    });
+
+    it('should encode some assorted test cases correctly', function () {
+        expect('n is 4 or n is not 6', 'to encode to', 'n === 4 || n !== 6');
+        expect(' n  is  4  or  n  is  not  6 ', 'to encode to', 'n === 4 || n !== 6');
+        expect('n within 2..4, 4..6', 'to encode to', 'n >= 2 && n <= 4 || n >= 4 && n <= 6');
+        expect('n in 2..4, 4..6', 'to encode to', 'n === Math.floor(n) && (n >= 2 && n <= 4 || n >= 4 && n <= 6)');
+        expect('n within 2, 4', 'to encode to', 'n === 2 || n === 4');
+        expect('n is not 2', 'to encode to', 'n !== 2');
+        expect('n mod 100 in 11..99', 'to encode to', 'n % 100 === Math.floor(n % 100) && n % 100 >= 11 && n % 100 <= 99');
+    });
 });
