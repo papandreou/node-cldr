2b73b632904bed269fe1b72568969a98227a5a58
Daniel Friesen <daniel@nadir-seen-fire.com>
2013-12-05 18:44:05 -0800

    Implement missing support for a number of missing cldr elements.
    Add a number of methods covering parts of cldr’s main .xml files that weren’t supported before.
    - extractLocaleDisplayPattern that covers /ldml/localeDisplayNames/localeDisplayPattern/*
    - extractKeyTypes that covers /ldml/localeDisplayNames/keys/key and /ldml/localeDisplayNames/types/type.
    - extractTransformNames that covers /ldml/localeDisplayNames/transformNames/transformName.
    - extractMeasurementSystemNames that covers /ldml/localeDisplayNames/measurementSystemNames/measurementSystemName.
    - extractCodePatterns that covers /ldml/localeDisplayNames/codePatterns/codePattern.
    
    Also make some modifications:
    - Expand extractTimeZoneFormats to contain a regions object with the daylight and standard types of regionFormat elements.
    - Put all the foo-bar -> fooBar code into a normalizeProperty function.
    - Add tests for all the cldr functions added or modified.
    - Do a tiny bit of tabs -> spaces changes in the tests.

--- test/extractFields.js
+++ test/extractFields.js
@@ -3,7 +3,7 @@ var expect = require('unexpected'),
 
 describe('extractFields', function () {
     it('should extract the British English fields correctly', function () {
-		var britishFields = cldr.extractFields('en_GB');
+        var britishFields = cldr.extractFields('en_GB');
         expect(britishFields, 'to only have keys', ['dayperiod', 'era', 'year', 'month', 'week', 'day', 'weekday', 'hour', 'minute', 'second', 'zone', 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']);
         expect(britishFields.dayperiod, 'to equal', {displayName: 'am/pm'});
         expect(britishFields.sat, 'to equal', {
