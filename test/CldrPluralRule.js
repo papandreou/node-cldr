6b1b43f4184465b6a57500cb03a60fbbceed9a5e
Andreas Lind <andreas@one.com>
2014-09-28 01:31:23 +0200

    CldrPluralRule: n = 1..10 should have 'in' semantics, not 'within'.
    Fixes #19

--- test/CldrPluralRule.js
+++ test/CldrPluralRule.js
@@ -16,6 +16,7 @@ describe('CldrPluralRule', function () {
         expect(' n  is  4  or  n  is  not  6 ', 'to encode to', 'n === 4 || n !== 6');
         expect('n within 2..4, 4..6', 'to encode to', 'n >= 2 && n <= 4 || n >= 4 && n <= 6');
         expect('n in 2..4, 4..6', 'to encode to', 'n === Math.floor(n) && (n >= 2 && n <= 4 || n >= 4 && n <= 6)');
+        expect('n = 2..4', 'to encode to', 'n === Math.floor(n) && n >= 2 && n <= 4');
         expect('n within 2, 4', 'to encode to', 'n === 2 || n === 4');
         expect('n is not 2', 'to encode to', 'n !== 2');
         expect('n mod 100 in 11..99', 'to encode to', 'n % 100 === Math.floor(n % 100) && n % 100 >= 11 && n % 100 <= 99');
--- test/CldrPluralRuleSet.js
+++ test/CldrPluralRuleSet.js
@@ -84,7 +84,7 @@ describe('CldrPluralRuleSet', function () {
                 var v = n.toString().replace(/^[^.]*\.?/, '').length,
                     f = parseInt(n.toString().replace(/^[^.]*\.?/, ''), 10);
                 if (typeof n === 'string') n = parseInt(n, 10);
-                if (n % 10 === 0 || (n % 100 >= 11 && n % 100 <= 19) || v === 2 && (f % 100 >= 11 && f % 100 <= 19)) return 'zero';
+                if (n % 10 === 0 || n % 100 === Math.floor(n % 100) && n % 100 >= 11 && n % 100 <= 19 || v === 2 && f % 100 === Math.floor(f % 100) && f % 100 >= 11 && f % 100 <= 19) return "zero";
                 if (n % 10 === 1 && (!(n % 100 === 11) || v === 2 && f % 10 === 1 && (!(f % 100 === 11) || !(v === 2) && f % 10 === 1))) return 'one';
                 return 'other';
             }
