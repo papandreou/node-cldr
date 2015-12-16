fcc8a7cf662fc203213a924a1606fc771a32c2c2
Maarten Winter <mw@one.com>
2015-12-16 17:01:37 +0100

    Add 'parent' to items in territoryContainmentGroups that have a parent, and ensure all regions have a parent that somehow is a child of region 001

--- test/extractTerritoryContainmentGroups.js
+++ test/extractTerritoryContainmentGroups.js
@@ -7,13 +7,16 @@ describe('extractTerritoryContainmentGroups', function () {
 		territoryContainmentGroups = cldr.extractTerritoryContainmentGroups();
 	});
 
-	it('does not contain the group 419, which is no direct descendant of world 001', function () {
-		expect(territoryContainmentGroups, 'not to have property', '419');
-	});
-
 	it('contains group 029, which is a descendant of 019, which is a descendant of 001', function () {
 		expect(territoryContainmentGroups, 'to have properties', ['029', '019', '001']);
 	});
 
-	
+	it('only contains groups that are descendants of group 001', function () {
+		var isContainedByType = {};
+
+		expect(Object.keys(territoryContainmentGroups), 'to have items satisfying', function (index) {
+			var item = territoryContainmentGroups[index];
+			expect((index === '001' || (('parent' in item) && Boolean(item.parent))), 'to be', true);
+        });
+	});
 });
