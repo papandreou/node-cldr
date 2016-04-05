7c7363d0611ad9a2b24db9c0b4a793d815dfa313
Andreas Lind <andreas@one.com>
2016-04-05 21:30:21 +0200

    Fix whitespace in test

--- test/extractPluralRuleFunction.js
+++ test/extractPluralRuleFunction.js
@@ -3,14 +3,14 @@ var expect = require('unexpected'),
 
 describe('extractPluralRuleFunction', function () {
     it('should extract the Romanian plural rule function correctly', function () {
-		var romanianPluralRule = cldr.extractPluralRuleFunction('ro');
+        var romanianPluralRule = cldr.extractPluralRuleFunction('ro');
         expect(
             romanianPluralRule.toString(),
             'to equal',
             'function anonymous(n) {\n' +
                 'var i=Math.floor(Math.abs(n)),v=n.toString().replace(/^[^.]*\\.?/,"").length;if(typeof n==="string")n=parseInt(n,10);if(i===1&&v===0)return"one";if(!(v===0)||n===0||!(n===1)&&n%100===Math.floor(n%100)&&n%100>=1&&n%100<=19)return"few";return"other"\n' +
             '}'
-            );
+        );
     });
 
     it('should build a Danish plural rule function that considers the number 0 to be in the "other" category', function () {
