4bac9280197fb622bf47c12f305a016a86c596ca
Andreas Lind <andreas@one.com>
2016-08-13 00:54:11 +0200

    Use unexpected-function-equality to improve the readability of the extractPluralRuleFunction test.

--- test/extractPluralRuleFunction.js
+++ test/extractPluralRuleFunction.js
@@ -1,16 +1,17 @@
-var expect = require('unexpected'),
+var expect = require('unexpected').clone().use(require('unexpected-function-equality')),
     cldr = require('../lib/cldr');
 
 describe('extractPluralRuleFunction', function () {
     it('should extract the Romanian plural rule function correctly', function () {
         var romanianPluralRule = cldr.extractPluralRuleFunction('ro');
-        expect(
-            romanianPluralRule.toString(),
-            'to equal',
-            'function anonymous(n) {\n' +
-                'var i=Math.floor(Math.abs(n)),v=n.toString().replace(/^[^.]*\\.?/,"").length;if(typeof n==="string")n=parseInt(n,10);if(i===1&&v===0)return"one";if(!(v===0)||n===0||!(n===1)&&n%100===Math.floor(n%100)&&n%100>=1&&n%100<=19)return"few";return"other"\n' +
-            '}'
-        );
+        expect(romanianPluralRule, 'to equal', function anonymous(n) {
+            var i = Math.floor(Math.abs(n)),
+                v = n.toString().replace(/^[^.]*\.?/,"").length;
+            if (typeof n === 'string') n = parseInt(n, 10);
+            if (i === 1 && v === 0) return 'one';
+            if (!(v === 0) || n === 0 || !(n === 1) && n % 100 === Math.floor(n % 100) && n % 100 >=1 && n % 100 <= 19) return'few';
+            return 'other';
+        });
     });
 
     it('should build a Danish plural rule function that considers the number 0 to be in the "other" category', function () {
