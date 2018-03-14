bf68e756667ee21e1d2c87b8afe7e10cc5d2a638
philippspo <philipp.sporrer@commercetools.de>
2018-03-14 15:56:52 +0100

    feat(extractors): add function to extract language metadata

---
+++ test/extractLanguageSupplementalMetadata.js
@@ -0,0 +1,21 @@
+var expect = require('unexpected'),
+    cldr = require('../lib/cldr');
+
+describe('extractLanguageSupplementalMetadata', function () {
+    it('should return an object with locale ids as keys and objects as values', function () {
+        expect(cldr.extractLanguageSupplementalMetadata(), 'to satisfy', {
+            no: {},
+            glv: {}
+        });
+    });
+    it('should return a reason', function () {
+        expect(cldr.extractLanguageSupplementalMetadata(), 'to satisfy', {
+            no: { replacement: 'nb' },
+        });
+    });
+    it('should return a replacement', function () {
+        expect(cldr.extractLanguageSupplementalMetadata(), 'to satisfy', {
+            no: { reason: 'legacy' },
+        });
+    });
+});
