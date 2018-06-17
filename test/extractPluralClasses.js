21e46baeb7c52c3102703f34a736e8a0e2ce8d92
rotsee <leo_wallentin@hotmail.com>
2018-06-17 23:04:07 +0200

    add extractPluralClasses

---
+++ test/extractPluralClasses.js
@@ -0,0 +1,20 @@
+var expect = require('unexpected').clone().use(require('unexpected-function-equality')),
+    cldr = require('../lib/cldr');
+
+describe('extractPluralClasses', function () {
+    it('should list three cardinal plural classes for Romanian', function () {
+        let romanianPluralClasses = cldr.extractPluralClasses('ro');
+        expect(romanianPluralClasses, 'to equal', ['one', 'few', 'other']);
+    });
+
+    it('should list two ordinal plural classes for Romanian', function () {
+        let romanianPluralClasses = cldr.extractPluralClasses('ro', 'ordinal');
+        expect(romanianPluralClasses, 'to equal', ['one', 'other']);
+    });
+
+    it('should list only one class for Yoruba', function () {
+        let yorubanPluralClasses = cldr.extractPluralClasses('yo');
+        expect(yorubanPluralClasses, 'to equal', ['other']);
+    });
+
+});
