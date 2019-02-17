4e326b58f745d907a25f589e9b3ae7f7dd25e73f
Andreas Lind <andreaslindpetersen@gmail.com>
2019-02-17 19:59:53 +0100

    Throw when attempting to extract data for a locale that isn't in CLDR
    Fixes #23

---
+++ test/cldr.js
@@ -0,0 +1,44 @@
+const expect = require('unexpected');
+const cldr = require('../lib/cldr');
+
+const localeLessExtractors = new Set([
+  'extractWindowsZonesByTimeZone',
+  'extractWindowsZonesByName',
+  'extractTerritories',
+  'extractTerritoryInfo',
+  'extractTerritoryContainmentGroups',
+  'extractLanguageSupplementalData',
+  'extractLanguageSupplementalMetadata',
+  'extractNumberingSystem',
+  'extractDigitsByNumberSystemId'
+]);
+
+describe('cldr', function() {
+  describe('with invalid locale ids', function() {
+    for (const propertyName in cldr) {
+      if (
+        /^extract/.test(propertyName) &&
+        typeof cldr[propertyName] === 'function' &&
+        !localeLessExtractors.has(propertyName)
+      ) {
+        describe(propertyName, function() {
+          it('should disallow extracting from a non-existent top-level locale', function() {
+            expect(
+              () => cldr[propertyName]('foobarquux'),
+              'to throw',
+              'No data for locale id: foobarquux'
+            );
+          });
+
+          it('should disallow extracting from a non-existent sublocale', function() {
+            expect(
+              () => cldr[propertyName]('da_foobarquux'),
+              'to throw',
+              'No data for locale id: da_foobarquux'
+            );
+          });
+        });
+      }
+    }
+  });
+});
