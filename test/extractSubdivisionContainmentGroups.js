36f33ea61d37f88f00dc89500e5cba9d5f34889a
rotsee <mejl@leowallentin.se>
2019-04-11 19:57:20 +0200

    code style

--- test/extractSubdivisionContainmentGroups.js
+++ test/extractSubdivisionContainmentGroups.js
@@ -8,13 +8,9 @@ describe('extractSubdivisionContainmentGroups', () => {
   });
 
   it('contains the chain UG > ugn > ug334', () => {
-    expect(subdivisionContainmentGroups, 'to have properties', [
-      'UG',
-      'ugn',
-    ]);
+    expect(subdivisionContainmentGroups, 'to have properties', ['UG', 'ugn']);
     expect(subdivisionContainmentGroups.UG.contains, 'to contain', 'ugn');
     expect(subdivisionContainmentGroups.ugn.contains, 'to contain', 'ug334');
     expect(subdivisionContainmentGroups.ugn.parent, 'to equal', 'UG');
   });
-
 });
--- test/extractTerritoryDisplayNames.js
+++ test/extractTerritoryDisplayNames.js
@@ -2,24 +2,22 @@ const expect = require('unexpected');
 const cldr = require('../lib/cldr');
 
 describe('extractTerritoryDisplayNames', () => {
-
   it('should export an object code - name dictionary', () => {
-    const territories = cldr.extractTerritoryDisplayNames('en')
+    const territories = cldr.extractTerritoryDisplayNames('en');
     expect(territories, 'to have keys satisfying', /[A-Z0-9]{2,3}/);
   });
 
   it('should contain localized names', () => {
-    const territoriesEn = cldr.extractTerritoryDisplayNames('en')
-    const territoriesSv = cldr.extractTerritoryDisplayNames('sv')
+    const territoriesEn = cldr.extractTerritoryDisplayNames('en');
+    const territoriesSv = cldr.extractTerritoryDisplayNames('sv');
     expect(territoriesEn.MK, 'to equal', 'North Macedonia');
     expect(territoriesSv.MK, 'to equal', 'Nordmakedonien');
   });
-
 });
 
 describe('extractSubdivisionDisplayNames', () => {
-  const subdivisionsEn = cldr.extractSubdivisionDisplayNames('en')
-  const subdivisionsSv = cldr.extractSubdivisionDisplayNames('sv')
+  const subdivisionsEn = cldr.extractSubdivisionDisplayNames('en');
+  const subdivisionsSv = cldr.extractSubdivisionDisplayNames('sv');
 
   it('should export an object code - name dictionary', () => {
     expect(subdivisionsEn, 'to have keys satisfying', /[A-Za-z0-9]{1,4}/);
@@ -29,5 +27,4 @@ describe('extractSubdivisionDisplayNames', () => {
     expect(subdivisionsEn.dk85, 'to equal', 'Zealand');
     expect(subdivisionsSv.dk85, 'to equal', 'Region Själland');
   });
-
 });
