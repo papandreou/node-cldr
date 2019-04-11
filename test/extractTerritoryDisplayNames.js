385a4d2d3fbebdce637c989d5e62053e84aec093
Andreas Lind <andreaslindpetersen@gmail.com>
2019-04-11 19:24:45 +0200

    typo
    Co-Authored-By: rotsee <mejl@leowallentin.se>

--- test/extractTerritoryDisplayNames.js
+++ test/extractTerritoryDisplayNames.js
@@ -21,7 +21,7 @@ describe('extractSubdivisionDisplayNames', () => {
   const subdivisionsEn = cldr.extractSubdivisionDisplayNames('en')
   const subdivisionsSv = cldr.extractSubdivisionDisplayNames('sv')
 
-  it('should export an object code - name dictionay', () => {
+  it('should export an object code - name dictionary', () => {
     expect(subdivisionsEn, 'to have keys satisfying', /[A-Za-z0-9]{1,4}/);
   });
 
