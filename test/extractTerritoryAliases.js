4c96b4133cb06617c75a23937d0d6ba254cd524b
rotsee <mejl@leowallentin.se>
2019-05-19 14:40:41 +0200

    run prettier

--- test/extractTerritoryAliases.js
+++ test/extractTerritoryAliases.js
@@ -8,14 +8,13 @@ describe('extractTerritoryAliases', () => {
   });
 
   it('contains territory aliases', () => {
-    expect(territoryAliases, 'to have properties', [
-      'AN',
-      '062',
-      'BLZ'
-    ]);
+    expect(territoryAliases, 'to have properties', ['AN', '062', 'BLZ']);
   });
   it('aliases are arrays of replacement rules', () => {
-    expect(territoryAliases.BU, 'to have properties', ['replacement', 'reason']);
+    expect(territoryAliases.BU, 'to have properties', [
+      'replacement',
+      'reason'
+    ]);
     expect(territoryAliases.BU.replacement, 'to equal', 'MM');
     expect(territoryAliases.AN.replacement.split(' '), 'to have length', 3);
   });
@@ -28,17 +27,16 @@ describe('extractSubdivisionAliases', () => {
   });
 
   it('contains territory aliases', () => {
-    expect(subdivisionAliases, 'to have properties', [
-      'fi01',
-      'cn11',
-      'lug'
-    ]);
+    expect(subdivisionAliases, 'to have properties', ['fi01', 'cn11', 'lug']);
   });
   it('aliases are replacement rules', () => {
-    expect(subdivisionAliases.fi01, 'to have properties', ['replacement', 'reason']);
+    expect(subdivisionAliases.fi01, 'to have properties', [
+      'replacement',
+      'reason'
+    ]);
     expect(subdivisionAliases.fi01.replacement, 'to equal', 'AX');
     expect(subdivisionAliases.fi01.reason, 'to equal', 'overlong');
     expect(subdivisionAliases.cn11.reason, 'to equal', 'deprecated');
-    expect(subdivisionAliases.lug.replacement.split(" "), 'to have length', 3);
+    expect(subdivisionAliases.lug.replacement.split(' '), 'to have length', 3);
   });
 });
