e7f940c1bd3911121f30a0f24ba7843f85a20bee
Andreas Lind <andreaslindpetersen@gmail.com>
2019-07-13 12:24:45 +0200

    eslint --fix .

--- test/extractPluralClasses.js
+++ test/extractPluralClasses.js
@@ -6,17 +6,17 @@ const cldr = require('../lib/cldr');
 
 describe('extractPluralClasses', () => {
   it('should list three cardinal plural classes for Romanian', () => {
-    let romanianPluralClasses = cldr.extractPluralClasses('ro');
+    const romanianPluralClasses = cldr.extractPluralClasses('ro');
     expect(romanianPluralClasses, 'to equal', ['one', 'few', 'other']);
   });
 
   it('should list two ordinal plural classes for Romanian', () => {
-    let romanianPluralClasses = cldr.extractPluralClasses('ro', 'ordinal');
+    const romanianPluralClasses = cldr.extractPluralClasses('ro', 'ordinal');
     expect(romanianPluralClasses, 'to equal', ['one', 'other']);
   });
 
   it('should list only one class for Yoruba', () => {
-    let yorubanPluralClasses = cldr.extractPluralClasses('yo');
+    const yorubanPluralClasses = cldr.extractPluralClasses('yo');
     expect(yorubanPluralClasses, 'to equal', ['other']);
   });
 });
