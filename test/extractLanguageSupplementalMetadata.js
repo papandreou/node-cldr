f20f7254571769acd67882d266f06214ff8709b3
Andreas Lind <andreas.lind@workday.com>
2021-04-19 23:00:55 +0200

    Adjust tests a bit

--- test/extractLanguageSupplementalMetadata.js
+++ test/extractLanguageSupplementalMetadata.js
@@ -5,18 +5,20 @@ const cldr = require('../lib/cldr');
 describe('extractLanguageSupplementalMetadata', () => {
   it('should return an object with locale ids as keys and objects as values', () => {
     expect(cldr.extractLanguageSupplementalMetadata(), 'to satisfy', {
-      no: {},
+      und_nynorsk: { replacement: 'und', reason: 'deprecated' },
       glv: {},
     });
   });
+
   it('should return a reason', () => {
     expect(cldr.extractLanguageSupplementalMetadata(), 'to satisfy', {
-      no: { replacement: 'nb' },
+      und_nynorsk: { replacement: 'und', reason: 'deprecated' },
     });
   });
+
   it('should return a replacement', () => {
     expect(cldr.extractLanguageSupplementalMetadata(), 'to satisfy', {
-      no: { reason: 'legacy' },
+      no_nyn: { replacement: 'nn', reason: 'deprecated' },
     });
   });
 });
